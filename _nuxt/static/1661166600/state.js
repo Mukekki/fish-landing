9ccd9c6b432436ed04bbc43eb4b588839fdf6140
Дмитрий Коровин <korovin@Dmitrijs-MacBook-Air.local>
2022-08-22 14:10:33 +0300

    init

---
+++ _nuxt/static/1661166600/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffish-landing\u002F_nuxt\u002Fstatic\u002F1661166600",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ffish-landing\u002F",assetsPath:"\u002Ffish-landing\u002F_nuxt\u002F",cdnURL:a}}}}(null));
